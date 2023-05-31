549f0b3a6ebdd8307e856517ac7748b006a0afa3
K4nishkk <115213375+K4nishkk@users.noreply.github.com>
2023-05-31 08:40:19 +0530

    * and ostream operator overloading

--- matrix.cpp
+++ matrix.cpp
@@ -3,28 +3,47 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> mulitply(vector<vector<int>> &m1, vector<vector<int>> &m2) {
-    // check compatibility
-    if (m1[0].size() != m2.size()) { // 1st matrix column == 2nd matrix rows
-        cout << "matrix multiplication not possible\n";
-        return {};
-    }
+class Matrix{
+public:
+    vector<vector<int>> m;
 
-    int n{static_cast<int>(m2.size())};
+    // constructor
+    explicit Matrix(vector<vector<int>> vec)
+        : m{vec} {}
 
-    int rows{static_cast<int>(m1.size())}, columns{static_cast<int>(m2[0].size())}; // dimensions of final matrix
+    // overloading * operator
+    Matrix operator* (Matrix const &mat) {
+        vector<vector<int>> m1{this -> m}, m2{mat.m};
 
-    vector<vector<int>> vec(rows, vector<int>(columns, 0));
-
-    for (int i{}; i < rows; i++) {
-        for (int j{}; j < columns; j++) {
-            for (int k{}; k < n; k++) {
-                vec[i][j] += m1[i][k] * m2[k][j];
-            }
+        if (m1[0].size() != m2.size()) {
+            cout << "Matrix multiplication unsuccessfull\n";
+            return Matrix({});
         }
+
+        int rows =static_cast<int>(m1.size());
+        int columns = static_cast<int>(m2[0].size());
+        int n = static_cast<int>(m2.size());
+
+        vector<vector<int>> vec(rows, vector<int>(columns, 0));
+
+        for (int i{}; i < rows; i++)
+            for (int j{}; j < columns; j++)
+                for (int k{}; k < n; k++)
+                    vec[i][j] += m1[i][k] * m2[k][j];
+        
+        return Matrix(vec);
+    }
+};
+
+// overloading stream insertion operator
+ostream & operator << (ostream &out, Matrix const &mat) {
+    for (vector<int> vec : mat.m) {
+        for (int element : vec)
+            out << element << "   ";
+        out << endl;
     }
 
-    return vec;
+    return out;
 }
 
 int main() {
@@ -39,12 +58,7 @@ int main() {
            {7, 8, 9},
            {7, 8, 9}};
 
-    vector<vector<int>> vec = mulitply(m1, m2);
-
-    for (vector<int> rows : vec) {
-        for (int element : rows) {
-            cout << element << "   ";
-        }
-        cout << endl;
-    }
+    Matrix mat1(m1), mat2(m2);
+    Matrix mat3 = mat1 * mat2;
+    cout << mat3;
 }
